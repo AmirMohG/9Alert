5996ebf53a698104d736e5425e383a7d4b6e58dd
Your Name <you@example.com>
2024-07-19 22:52:39 +0330

    first commit

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import os
 import telebot
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
-TOKEN = os.getenv('TOKEN', 'token')
-USER_ID = os.getenv('USER_ID', 'uid')
 
 load_dotenv()
+TOKEN = os.getenv('TOKEN', 'token')
+USER_ID = os.getenv('USER_ID', 'user_id')
+
 bot = telebot.TeleBot(TOKEN)
 
 # Create a Flask web server
@@ -17,10 +18,16 @@ def send_message():
     data = request.json
     print(f"Received POST data: {data}")
 
-    # Optionally, send a message to the USER_ID with the received data
-    bot.send_message(USER_ID, f"Received data: {data}")
+    # Extract the value of "id"
+    msg = [alert['annotations']['msg'] for alert in data.get('alerts', [])]
+    print(f"Extracted ids: {msg}")
 
-    return jsonify({"message": "Message sent to the user!", "data_received": data}), 200
+    # Optionally, send a message to the USER_ID with the extracted ids
+    if msg:
+        for msg_value in msg:
+            bot.send_message(USER_ID, f"Extracted id: {msg_value}")
+
+    return jsonify({"message": "Message sent to the user!", "msg": msg}), 200
 
 # Start the Flask web server on port 5000
 if __name__ == '__main__':
